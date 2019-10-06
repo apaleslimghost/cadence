fbd8062e26efc4307afd5e16ae45e051153fcca6
bren <bren@153.io>
2019-10-06 15:31:33 +0100

    multiple editors

--- packages/app/client.js
+++ packages/app/client.js
@@ -2,18 +2,33 @@ import CodeMirror from 'codemirror'
 import parinfer from 'parinfer-codemirror'
 import compiler from '@cadence/compiler'
 
-const cm = CodeMirror.fromTextArea(document.getElementById('editor'), {
-    mode: 'text/x-common-lisp',
-    tabSize: 3,
-    indentWithTabs: true,
-    autofocus: true,
-    dragDrop: false,
-})
-
-parinfer.init(cm)
-
 const output = document.getElementById('output')
+const editor = document.getElementById('editor')
+const editors = []
 
-cm.on('change', () => {
-    output.innerHTML = JSON.stringify(compiler(cm.getValue()), null, 2)
-})
+function compile() {
+    output.innerHTML = JSON.stringify(
+        editors.map(editor => compiler(editor.getValue())),
+        null, 2
+    )
+}
+
+function createEditor() {
+    const cm = CodeMirror(editor, {
+        mode: 'text/x-common-lisp',
+        tabSize: 3,
+        indentWithTabs: true,
+        autofocus: true,
+        dragDrop: false,
+        extraKeys: {
+            'Shift-Enter': createEditor,
+        }
+    })
+
+    parinfer.init(cm)
+    cm.on('change', compile)
+    editors.push(cm)
+}
+
+createEditor()
+
