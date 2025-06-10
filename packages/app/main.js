0cfef0a46177f1bb7fca95aa7c417e6db18096a0
Kara Brightwell <kara@153.io>
2025-06-10 14:20:59 +0100

    remove athloi and delete app package

--- packages/app/main.js
+++
@@ -1,9 +0,0 @@
-const { app, BrowserWindow } = require('electron')
-
-app.on('ready', () => {
-    const window = new BrowserWindow({
-        titleBarStyle: 'hidden'
-    })
-
-    window.loadURL('http://localhost:1234')
-})
