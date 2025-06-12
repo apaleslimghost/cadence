cfa21c3d97f12a6508b6d9971c70bf927ee3907c
Kara Brightwell <kara@153.io>
2025-06-12 09:52:18 +0100

    set up handsontable

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,24 +1,36 @@
 import { useState } from 'react'
-import './App.css'
 import evaluate from '@cadence/compiler'
+import {HotTable} from '@handsontable/react-wrapper';
+
 
 function App() {
-  const [code, setCode] = useState('')
+  // const [code, setCode] = useState('')
 
-  let result;
+  // let result;
 
-  try {
-    result = evaluate(code)
-  } catch(e) {
-    result = (e as Error).message
-  }
+  // try {
+  //   result = evaluate(code)
+  // } catch(e) {
+  //   result = (e as Error).message
+  // }
 
-  return (
-    <>
-      <textarea value={code} onChange={e => setCode(e.target.value)} />
-      <pre>{JSON.stringify(result)}</pre>
-    </>
-  )
+  return <div className="ht-theme-main-dark-auto">
+    <HotTable
+      data={[
+        ['', 'Tesla', 'Volvo', 'Toyota', 'Ford'],
+        ['2019', 10, 11, 12, 13],
+        ['2020', 20, 11, 14, 13],
+        ['2021', 30, 15, 12, 13]
+      ]}
+      rowHeaders={true}
+      colHeaders={true}
+      height="auto"
+      autoWrapRow={true}
+      autoWrapCol={true}
+      licenseKey="non-commercial-and-evaluation"
+    />
+  </div>
+
 }
 
 export default App
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
+import 'handsontable/styles/handsontable.min.css';
+import 'handsontable/styles/ht-theme-main.min.css';
+
 import App from './App.tsx'
+
+import { registerAllModules } from 'handsontable/registry';
+registerAllModules();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
