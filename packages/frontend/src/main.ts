71feba096068a46f0d315e9a38627664601bf394
Kara Brightwell <kara@153.io>
2025-06-27 21:44:03 +0100

    let code die

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -4,7 +4,6 @@ import './index.css';
 
 import Handsontable from 'handsontable';
 import { registerAllModules } from 'handsontable/registry';
-import { asyncScheduler, BehaviorSubject, combineLatest, concat, defer, delay, filter, finalize, from, interval, isObservable, map, mergeMap, Observable, observeOn, of, repeat, scan, share, shareReplay, Subscriber, Subscription, switchMap, tap, withLatestFrom, zip, type OperatorFunction } from 'rxjs';
 import pick from 'lodash/pick'
 import mapValues from 'lodash/mapValues';
 
@@ -16,23 +15,6 @@ import { get, partition, sortBy, takeWhile } from 'lodash';
 registerAllModules();
 
 const ctx = new AudioContext()
-
-const cells: Record<string, BehaviorSubject<Observable<unknown>>> = {}
-const cellSubscriptions: Record<string, Subscription> = {}
-
-function getCell(key: string) {
-  return (cells[key] ??= new BehaviorSubject(of('' as unknown)))
-}
-
-function readCell(key: string) {
-  return getCell(key).pipe(
-    switchMap(o => o),
-  )
-}
-
-function replaceCell(key: string, cell: Observable<unknown>) {
-  getCell(key).next(cell)
-}
 
 const colLetter = (col: number): string =>
   col <= 0
@@ -147,148 +129,70 @@ Handsontable.cellTypes.registerCellType('lisp', {
     const cellKey = colLetter(column + 1) + (row + 1).toString(10)
 
     if(value) {
-      cellSubscriptions[cellKey] ??= readCell(cellKey).subscribe(
-        result => {
-          td.animate([
-            { background: '#80D8FF' },
-            { background: 'transparent' },
-          ], {
-            duration: 200,
-            easing: 'ease-out'
-          })
+        // result => {
+        //   td.animate([
+        //     { background: '#80D8FF' },
+        //     { background: 'transparent' },
+        //   ], {
+        //     duration: 200,
+        //     easing: 'ease-out'
+        //   })
 
-          if(isConnectable(result)) {
-            let canvas = td.querySelector('canvas')
-            if(!canvas) {
-              canvas = document.createElement('canvas')
-              canvas.width = td.clientWidth * devicePixelRatio
-              canvas.height = td.clientHeight * devicePixelRatio
-              td.replaceChildren(canvas)
-            }
-            const osc = new Oscilloscope(ctx, result, canvas)
-            osc.run()
-          } else if(result != null) {
-            td.textContent = result as string
-          } else {
-            td.textContent = '🔃 Empty'
-          }
-        }
-      )
+        //   if(isConnectable(result)) {
+        //     let canvas = td.querySelector('canvas')
+        //     if(!canvas) {
+        //       canvas = document.createElement('canvas')
+        //       canvas.width = td.clientWidth * devicePixelRatio
+        //       canvas.height = td.clientHeight * devicePixelRatio
+        //       td.replaceChildren(canvas)
+        //     }
+        //     const osc = new Oscilloscope(ctx, result, canvas)
+        //     osc.run()
+        //   } else if(result != null) {
+        //     td.textContent = result as string
+        //   } else {
+        //     td.textContent = '🔃 Empty'
+        //   }
+        // }
     } else {
-      cellSubscriptions[cellKey]?.unsubscribe()
-      delete cellSubscriptions[cellKey]
-      td.textContent = ''
+      // cellSubscriptions[cellKey]?.unsubscribe()
+      // delete cellSubscriptions[cellKey]
+      // td.textContent = ''
     }
   },
 });
 
 const root = document.getElementById('root')!
 
-type MaybeObsvervable<T> = Observable<T> | T
-const toObservable = <T>(v: MaybeObsvervable<T>) => isObservable(v) ? v : new BehaviorSubject(v)
-
 const rxlib = {
-  map: (obs: Observable<unknown>, fn: (a: unknown) => unknown) => obs.pipe(map(fn)),
-  of,
-  //@ts-expect-error idc
-  'pipe': (head: Observable<unknown>, ...tail: OperatorFunction<unknown, unknown>[]) => head.pipe(...tail),
-  interval(...args: Parameters<typeof interval>) {
-    return interval(...args).pipe(shareReplay(1))
-  },
-  clock: (bpm: number, ppqn: number = 4) => rxlib.interval(60 * 1000 / (bpm * ppqn)),
-  '*': (...args: MaybeObsvervable<number>[]) => combineLatest(args.map(toObservable)).pipe(
-    map((args) => args.reduce((a, b) => a * b))
-  ),
-  '//': (obs: Observable<unknown>, n: number) => obs.pipe(filter((_, i) => (i % n) === 0), share()),
-  seq: (trigs: Observable<unknown>, ...events: unknown[]) => zip(
-    from(events).pipe(
-      observeOn(asyncScheduler),
-      repeat()
-    ),
-    trigs
-  ).pipe(
-    map(([ev]) => ev),
-    share()
-  ),
-  '→': (dest: Observable<AudioNode>, ...sources: Observable<AudioNode>[]) => {
-    let sources_: AudioNode[]
-    return toObservable(dest).pipe(
-      switchMap(
-        dest => combineLatest(sources).pipe(
-          map(sources => {
-            sources.forEach(s => s?.connect(dest))
-            sources_ = sources
-            return dest
-          }),
-          finalize(() => sources_.forEach(s => s.disconnect(dest)))
-        )
-      )
-    )
+  '→': (dest: AudioNode, ...sources: AudioNode[]) => {
+    sources.forEach(s => s?.connect(dest))
+    return dest
+
+    // finalize(() => sources_.forEach(s => s.disconnect(dest)))
   },
   // TODO allow using Connectables for params
-  'osc': (type: OscillatorType, freq: MaybeObsvervable<number | Connectable>) => {
-    return defer(() => {
-      const osc = new OscillatorNode(ctx, { type })
-      osc.start()
-      return toObservable(freq).pipe(
-        map(f => (isConnectable(f) ? f.connect(osc.frequency) : osc.frequency.setValueAtTime(f, ctx.currentTime), osc)),
-        finalize(() => osc.stop())
-      )
-    })
+  'osc': (type: OscillatorType, freq: number | Connectable) => {
+    const osc = new OscillatorNode(ctx, { type })
+    osc.start()
+    if(isConnectable(freq)) {
+      freq.connect(osc.frequency)
+    } else {
+      osc.frequency.setValueAtTime(freq, ctx.currentTime)
+    }
+      // finalize(() => osc.stop())
+    return osc
   },
   'dest': () => {
     const gain = new GainNode(ctx)
     gain.connect(ctx.destination)
     return gain
   },
-  'gate': (clock: Observable<unknown>, length: MaybeObsvervable<number>) => clock.pipe(
-    switchMap(() => concat(
-      of(true),
-      toObservable(length).pipe(
-        mergeMap(ms => of(false).pipe(delay(ms)))
-      )
-    ))
-  ),
-  'ad': (trig: Observable<unknown>, a: MaybeObsvervable<number>, d: MaybeObsvervable<number>) => defer(() => {
-    const env = new ControlParam(ctx)
-    return trig.pipe(
-      switchMap(() => combineLatest([toObservable(a), toObservable(d)]).pipe(
-        map(([a, d]) => {
-          env.linearRampToValueAtTime(1, ctx.currentTime + a)
-          env.linearRampToValueAtTime(0, ctx.currentTime + d)
-        })
-      )),
-      map(() => env)
-    )
-  }),
-  'vca': (gain: MaybeObsvervable<number | Connectable>) => {
-    return defer(() => {
-      const node = new GainNode(ctx)
-      return toObservable(gain).pipe(
-        map(g => (isConnectable(g) ? g.connect(node.gain) : node.gain.setValueAtTime(g, ctx.currentTime), node))
-      )
-    })
-  },
-  'euclid': (trigs: Observable<unknown>, length: MaybeObsvervable<number>, steps: MaybeObsvervable<number>) => trigs.pipe(
-    scan(i => i + 1, -1),
-    withLatestFrom(toObservable(steps), toObservable(length)),
-    filter(([i, steps, length]) => (i * steps) % length < steps),
-    share()
-  ),
-  'note': (note: string) => {
-    const oct = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-    const A4 = 440
-    const index = oct.indexOf(note) - 9
-    return A4 * Math.pow(2, index/12)
-  }
-}
 
-const rxspec = {
-  subscribe: readCell
 }
 
 new Handsontable(root, {
-  className: "ht-theme-main-dark-auto",
+  className: "ht-theme-main-dark",
   cells: () => ({ type: 'lisp' }),
   minCols: 100,
   minRows: 100,
@@ -309,12 +213,12 @@ new Handsontable(root, {
       let result
 
       try {
-        result = evaluate(newValue, rxlib, rxspec)
+        result = evaluate(newValue, rxlib)
       } catch(error) {
         console.log(error)
       }
 
-      replaceCell(cellKey, toObservable(result))
+      // replaceCell(cellKey, toObservable(result))
     }
   },
   licenseKey: "non-commercial-and-evaluation"
