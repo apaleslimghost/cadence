4e1b87b1c28a5266582c604869aef692bc9432a9
Kara Brightwell <kara@153.io>
2025-12-09 14:41:15 +0000

    support top level await

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,9 @@ import {plugin as mdPlugin} from 'vite-plugin-markdown'
 import wasm from 'vite-plugin-wasm'
 
 export default {
+  build: {
+    target: 'esnext'
+  },
   plugins: [
     mdPlugin({
       mode: 'html'
